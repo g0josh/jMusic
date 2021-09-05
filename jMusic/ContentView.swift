cc9b9ba43f2e650db0bbfd1b8cf8ceb8e72b8539
Jobin Jose <jobin.jose@x15.com.au>
2021-09-05 19:33:14 +1000

    Initial Commit

---
+++ jMusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jMusic
+//
+//  Created by Jobin Jose on 5/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ jMusic/jMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  jMusicApp.swift
+//  jMusic
+//
+//  Created by Jobin Jose on 5/9/21.
+//
+
+import SwiftUI
+
+@main
+struct jMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
